bb7f74665cb6fbfb71d9c6c2d933e98ace9acc9e
Samin1615088 <samin.shafkat@gmail.com>
2021-05-25 14:08:40 +0600

    dependency added

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start</Text>
+      <Text>Open up App.js to starts</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+
+//     </NavigationContainer>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
